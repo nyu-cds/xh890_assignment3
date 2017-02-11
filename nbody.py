0c20e11ac2292420bb2380d5e65b490d81bf34c6
Xingyue Huang <xh890@nyu.edu>
2017-02-11 13:23:59 -0500

    Add running time to nbody.py

--- nbody.py
+++ nbody.py
@@ -1,5 +1,6 @@
 """
     N-body simulation.
+    Runtime: 114.92041752999648
 """
 
 PI = 3.14159265358979323
@@ -139,5 +140,6 @@ def nbody(loops, reference, iterations):
         print(report_energy())
 
 if __name__ == '__main__':
-    nbody(100, 'sun', 20000)
+    import timeit
+    print(timeit.timeit(lambda: nbody(100, 'sun', 20000), number=1))
 
