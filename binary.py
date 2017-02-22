30f90246241605f81575b5573899fa52997f1747
Xingyue Huang <xh890@nyu.edu>
2017-02-22 17:34:44 -0500

    Add some comments to binary.py

--- binary.py
+++ binary.py
@@ -1,8 +1,12 @@
 from itertools import product  # import itertools module
 def zbits(n, k):
-    arrays = [(0, 1) for _ in range(n)]
-    cp = list(product(*arrays))
+    arrays = [(0, 1) for _ in range(n)] # generate a list of n tuples (0,1)
+    cp = list(product(*arrays)) # generate permutations
+    # filter cp so that it only contains tuples that have k zeros
+    #  and convert cp to list
     cp = [list(item) for item in cp if sum(item)==n-k]
     for item in cp:
+        # convert intergers to strings for join
         item = [str(val) for val in item]
+        # print binary strings out
         print(''.join(item))
