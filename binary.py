c2ec0fdd818b958d6aa26e22c182923db3973f39
Xingyue Huang <xh890@nyu.edu>
2017-02-24 19:37:19 -0500

    Add assert test

--- binary.py
+++ binary.py
@@ -5,8 +5,16 @@ def zbits(n, k):
     # filter cp so that it only contains tuples that have k zeros
     #  and convert cp to list
     cp = [list(item) for item in cp if sum(item)==n-k]
+    output = set()
     for item in cp:
         # convert intergers to strings for join
         item = [str(val) for val in item]
         # print binary strings out
-        print(''.join(item))
+        output.add(''.join(item))
+    return output
+
+if __name__ == '__main__':
+    
+    assert zbits(4, 3) == {'0100', '0001', '0010', '1000'}
+    assert zbits(4, 1) == {'0111', '1011', '1101', '1110'}
+    assert zbits(5, 4) == {'00001', '00100', '01000', '10000', '00010'}
